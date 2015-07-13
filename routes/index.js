b027eebe6f69e5829705d45b62dbfcde5d841c6f
Pedro <pedro_ip@hotmail.com>
2015-07-13 11:53:47 +0200

    Primera pregunta

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+/* Cargamos el nuevo Controlador */
+var quizController = require('../controllers/quiz_controller');
+
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) { 
+  /* Renderiza la Vista Quiz sin controlador  */	
+  res.render('index', { title: 'Quiz' });
 });
 
+/* Añadimos nuevas rutas que llaman a Controladores */
+router.get('/quizes/question',quizController.question);
+router.get('/quizes/answer',quizController.answer);
+
+ 
 module.exports = router;
